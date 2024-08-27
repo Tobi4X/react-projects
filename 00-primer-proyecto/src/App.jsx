f55c191988917d7d778f6401b0bb428677e78900
Tobias <tobiasjensen04@gmail.com>
2024-08-27 20:17:50 -0300

    final de primer proyecto, creo un componente por primera vez y utilizo el useState para que un boton permita o no ver una imagen

--- 00-primer-proyecto/src/App.jsx
+++ 00-primer-proyecto/src/App.jsx
@@ -1,13 +1,11 @@
 import { BotonImagen } from "./components/BotonImagen.jsx"
 
-const imagen = "../assets/react.svg"
-
 export default function App () {
     return (
         <>
             <div>
-                <BotonImagen texto="Hola" />
-                <BotonImagen texto="Chau" imagen={imagen}/>
+                <BotonImagen />
+                <BotonImagen />
             </div>
             
         </>
--- 00-primer-proyecto/src/components/BotonImagen.jsx
+++ 00-primer-proyecto/src/components/BotonImagen.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react"
 
-export function BotonImagen ({texto, imagen}) {
+
+export function BotonImagen ({}) {
+    
+    const [showImagen, setShowImagen] = useState(false)
+
+    const texto = showImagen ? "Ocultar Imagen" : "Mostrar Imagen"
+    const imagenImg = showImagen ? <img src={'https://unavatar.io/github/tobi4x'} alt="imagen" /> : <div></div>
+
+    const handleClick = () => {
+        setShowImagen(!showImagen)
+    }
+
     return(
         <div>
-            <img src={imagen} alt="imagen del boton" />
-            <button>{texto}</button>
+            {imagenImg}
+            <button onClick={handleClick}>{texto}</button>
         </div>
     )
 }
